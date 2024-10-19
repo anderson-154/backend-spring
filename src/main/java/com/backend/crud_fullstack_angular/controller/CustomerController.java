689f9211b0b8c9a27ec66489b0def99a640556f6
Anderson <andyguaca@gmail.com>
2024-10-19 17:07:52 -0500

    feat: allow cross-origin rquest from frontend with @CrossOrigin

--- src/main/java/com/backend/crud_fullstack_angular/controller/CustomerController.java
+++ src/main/java/com/backend/crud_fullstack_angular/controller/CustomerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 @RestController
 @RequestMapping("/api/customers")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CustomerController {
     @Autowired
     private CustomerSeviceImpl customerSeviceImpl;
